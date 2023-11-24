2ed5bc8c802eb0921ee23a9b4b62cb439236c685
NPS <nicolaspetri18@gmail.com>
2023-11-24 10:53:22 -0300

    começando

---
+++ clientes.js
@@ -0,0 +1,54 @@
+var clientes = {
+    usuários: [{
+            id: 1,
+            nome: "Nicolas Petri",
+            email: "nicolaspetri18@gmail.com",
+            telefone: 11941221523,
+            senha: "nicolas1234",
+            foto_perfil: "",
+            endereco: [{
+                bairro: "Primeiro de Maio",
+                cep: 6365720,
+                rua: "Rua Bósnia",
+                numero: 25,
+                cidade: "Carapicuiba",
+                estado: "São Paulo",
+                complemento: "casa"
+            }]
+        },
+        {
+            id: 2,
+            nome: "Maria Caroline",
+            email: "maria@gmail.com",
+            telefone: 11912344321,
+            senha: "maria1234",
+            foto_perfil: "",
+            endereco: [{
+                cep: 1001001,
+                bairro: "São Paulo",
+                rua: "Praça da Sé",
+                numero: 0,
+                cidade: "São Paulo",
+                estado: "São Paulo",
+                complemento: ""
+            }]
+        },
+        {
+            id: 3,
+            nome: "Gustavo Henrique",
+            email: "gustavo@gmail.com",
+            telefone: 11987654321,
+            senha: "gustavo1234",
+            foto_perfil: "",
+            endereco: [{
+                cep: 1001001,
+                bairro: "São Paulo",
+                rua: "Avenida Presidente Juscelino Kubitschek",
+                numero: 1909,
+                cidade: "São Paulo",
+                estado: "São Paulo",
+                complemento: "Bloco 4, 19º Andar"
+            }]
+        }
+    ]
+}
